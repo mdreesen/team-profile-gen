0f0e60e43249e1ee84d35d007e21859f7c92275a
mdreesen <56687081+mdreesen@users.noreply.github.com>
2020-09-24 19:50:49 -0600

    added code to tests and required inquirer for prompts for the manager information

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const inquirer = require('inquirer');
-const generate = require('./template');
+const Manager = require('./lib/Manager');
 
-const team = () => {
-    console.log(`
-    ====================================
-    Please Provide your team information
-    ====================================
-    `)
-
+function Profile() {
+    this.Manager;
 }
+
+Profile.prototype.start = function() {
+    console.log(`
+        ===================
+        Manager Information
+        ===================
+        `)
+    return inquirer.prompt({
+            type: 'input',
+            name: 'Manager Name',
+            message: "What is your Manager's name?"
+        }, {
+            type: 'input',
+            name: 'Manager ID',
+            message: "What is your Manager's ID?"
+        }, {
+            type: 'input',
+            name: 'Manager office number',
+            message: "WHat is your Manager's office number?"
+        })
+        .then(({ name }) => {
+            this.manager = new Manager(name);
+
+            this.viewProfile();
+        })
+};
+
+module.exports = Profile();
