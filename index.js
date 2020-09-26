0f2a4c7efb12f22197735a488871297278dc5146
mdreesen <56687081+mdreesen@users.noreply.github.com>
2020-09-25 19:31:25 -0600

    have it asking all the questions

--- index.js
+++ index.js
@@ -6,29 +6,21 @@ function Start() {
         ===================
         Manager Information
         ===================
-        `)
+        `);
+
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'Manager Name',
+        message: "What is your Manager's name?"
+    }, {
+        type: 'input',
+        name: 'Manager ID',
+        message: "What is your Manager's ID?"
+    }, {
+        type: 'input',
+        name: 'Manager office number',
+        message: "What is your Manager's office number?"
+    }])
 };
-
-Start.prototype.mQuestions = function() {
-    return inquirer.prompt({
-            type: 'input',
-            name: 'Manager Name',
-            message: "What is your Manager's name?"
-        }, {
-            type: 'input',
-            name: 'Manager ID',
-            message: "What is your Manager's ID?"
-        }, {
-            type: 'input',
-            name: 'Manager office number',
-            message: "WHat is your Manager's office number?"
-        })
-        .then(({ name }) => {
-            this.manager = new Manager(name);
-
-            this.viewProfile();
-        })
-};
-
 
 module.exports = Start();
